518bc0c02cf213e9837414660067c5d21469771a
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-10 11:37:48 +0000

    input is appearing in html

--- calculator.js
+++ calculator.js
@@ -1,20 +1,9 @@
-// const key__0 = document.querySelector("#buttons__0")
-// const key__1 = document.querySelector("#buttons__1")
-// const key__2 = document.querySelector("#buttons__2")
-// const key__3 = document.querySelector("#buttons__3")
-// const key__4 = document.querySelector("#buttons__4")
-// const key__5 = document.querySelector("#buttons__5")
-// const key__6 = document.querySelector("#buttons__6")
-// const key__7 = document.querySelector("#buttons__7")
-// const key__8 = document.querySelector("#buttons__8")
-// const key__9 = document.querySelector("#buttons__9")
-// const key__00 = document.querySelector("#buttons__00")
-// const key__decimal = document.querySelector("#buttons__decimal");
-// const key__equal = document.querySelector("#buttons__equal")
-// const key__add = document.querySelector("#buttons__add")
-// const key__subtract = document.querySelector("#buttons__subtract")
-// const key__multiply = document.querySelector("#buttons__multiply")
-// const key__divide = document.querySelector("#buttons__divide")
+const realDisplay = document.querySelector("#buttons__screen")
+const key__equal = document.querySelector("#buttons__equal")
+const key__add = document.querySelector("#buttons__add")
+const key__subtract = document.querySelector("#buttons__subtract")
+const key__multiply = document.querySelector("#buttons__multiply")
+const key__divide = document.querySelector("#buttons__divide")
 const key__delete = document.querySelector("#buttons__delete")
 const key__clear = document.querySelector("#buttons__clear")
 
@@ -24,44 +13,28 @@ const key__clear = document.querySelector("#buttons__clear")
 const keys = document.querySelectorAll(".key")
 const number = document.querySelectorAll(".number")
 const operation = document.querySelectorAll(".operation")
-const clear = document.querySelector(".clear")
-const remove = document.querySelector(".delete")
 
 let calculatorDisplay = [];
-
-// const keys = [key__0,key__1,key__2,key__3,key__4,key__5,key__6,key__7,key__8,key__9,key__00,key__decimal] 
-
-
 
 for (let i = 0; i< keys.length;i++) {
    keys[i].addEventListener("click", ()=>{
       calculatorDisplay.push(keys[i].innerHTML)
       console.log(calculatorDisplay)
-   })
+      realDisplay.innerHTML = `${calculatorDisplay.join("")}`
+})
 }
 
-// for (let i=0; i<operation.length; i++){
-//    operation[i].addEventListener("click", ()=>{
-//       calculatorDisplay.push(operation[i].innerHTML)
-//       console.log(calculatorDisplay)
-//    })
-// }
+// key__equal
 
-remove.addEventListener("click",()=> {
+
+key__delete.addEventListener("click",()=> {
    calculatorDisplay.pop();
    console.log("delete button pressed")
+   realDisplay.innerHTML = `${calculatorDisplay.join("")}`
 })
 
-clear.addEventListener("click",()=> {
+key__clear.addEventListener("click",()=> {
    console.log("clear button pressed")
    calculatorDisplay=[];
+   realDisplay.innerHTML = `${calculatorDisplay.join("")}`
 })
-
-// btn.addEventListener("click",()=>{
-//    calculatorDisplay.innerHTML = calculatorDisplay.value
-// })
-
-// console.log(keys)
-// const activeCalculatorDisplay = (keys) => {
-
-// } 
