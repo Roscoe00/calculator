b1f5c82914f51bd6df1bfdbcee6268eb7c6ae7aa
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-11 15:41:59 +0000

    changed input to a string

--- calculator.js
+++ calculator.js
@@ -1,4 +1,16 @@
 const realDisplay = document.querySelector("#buttons__screen")
+const key__0 = document.querySelector("#buttons__0")
+const key__1 = document.querySelector("#buttons__1")
+const key__2 = document.querySelector("#buttons__2")
+const key__3 = document.querySelector("#buttons__3")
+const key__4 = document.querySelector("#buttons__4")
+const key__5 = document.querySelector("#buttons__5")
+const key__6 = document.querySelector("#buttons__6")
+const key__7 = document.querySelector("#buttons__7")
+const key__8 = document.querySelector("#buttons__8")
+const key__9 = document.querySelector("#buttons__9")
+const key__00 = document.querySelector("#buttons__00")
+const key__decimal = document.querySelector("#buttons__decimal")
 const key__equal = document.querySelector("#buttons__equal")
 const key__add = document.querySelector("#buttons__add")
 const key__subtract = document.querySelector("#buttons__subtract")
@@ -15,26 +27,34 @@ const number = document.querySelectorAll(".number")
 const operation = document.querySelectorAll(".operation")
 
 let calculatorDisplay = [];
+let currentDisplay = "";
 
 for (let i = 0; i< keys.length;i++) {
    keys[i].addEventListener("click", ()=>{
-      calculatorDisplay.push(keys[i].innerHTML)
-      console.log(calculatorDisplay)
-      realDisplay.innerHTML = `${calculatorDisplay.join("")}`
-})
+      currentDisplay = `${currentDisplay}` + `${keys[i].innerHTML}`
+      if (keys[i]===key__add){       
+         calculatorDisplay.push(currentDisplay.substring(0, currentDisplay.length - 1))
+         calculatorDisplay.push(currentDisplay.substring(currentDisplay.length-1, currentDisplay.length))
+         currentDisplay="";
+         console.log(calculatorDisplay)
+         console.log("add button pressed")
+         
+      } else{
+         console.log(calculatorDisplay)
+      realDisplay.innerHTML = `${currentDisplay}`
+      }
+   })
 }
-
-// key__equal
-
+// possible way of removing last operator pressed?
+// calculatorDisplay = calculatorDisplay.splice(i,1);
 
 key__delete.addEventListener("click",()=> {
-   calculatorDisplay.pop();
+   currentDisplay=currentDisplay.substring(0, currentDisplay.length - 1);
    console.log("delete button pressed")
-   realDisplay.innerHTML = `${calculatorDisplay.join("")}`
+   realDisplay.innerHTML = `${currentDisplay}`
 })
 
 key__clear.addEventListener("click",()=> {
    console.log("clear button pressed")
-   calculatorDisplay=[];
-   realDisplay.innerHTML = `${calculatorDisplay.join("")}`
-})
+   currentDisplay="";
+   realDisplay.innerHTML = `${currentDisplay}`})
