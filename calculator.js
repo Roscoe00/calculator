fe0b70cf68e273ea54b0d4afd8fd8394b01e6597
Roscoe Bull <roscoe-bull@live.co.uk>
2021-11-14 22:37:30 +0000

    finished styling and fixed minus key

--- calculator.js
+++ calculator.js
@@ -1,54 +1,36 @@
-// const key__0 = document.querySelector("#buttons__0")
-// const key__1 = document.querySelector("#buttons__1")
-// const key__2 = document.querySelector("#buttons__2")
-// const key__3 = document.querySelector("#buttons__3")
-// const key__4 = document.querySelector("#buttons__4")
-// const key__5 = document.querySelector("#buttons__5")
-// const key__6 = document.querySelector("#buttons__6")
-// const key__7 = document.querySelector("#buttons__7")
-// const key__8 = document.querySelector("#buttons__8")
-// const key__9 = document.querySelector("#buttons__9")
-// const key__00 = document.querySelector("#buttons__00")
+const workingDisplay = document.querySelector("#buttons__subscreen")
+const finalDisplay = document.querySelector("#buttons__screen")
+
 const key__decimal = document.querySelector("#buttons__decimal")
 const key__minus = document.querySelector("#buttons__minus")
 
-const workingDisplay = document.querySelector("#buttons__subscreen")
-const finalDisplay = document.querySelector("#buttons__screen")
 const key__equal = document.querySelector("#buttons__equal")
 const key__add = document.querySelector("#buttons__add")
 const key__subtract = document.querySelector("#buttons__subtract")
 const key__multiply = document.querySelector("#buttons__multiply")
 const key__divide = document.querySelector("#buttons__divide")
+
 const key__delete = document.querySelector("#buttons__delete")
 const key__clear = document.querySelector("#buttons__clear")
 
-// maybe could have all numbers stored in an array and all operations in another
-// calculatorMemory.unshift(key[i])
-
-const keys = document.querySelectorAll(".key")
 const number = document.querySelectorAll(".number")
 const operation = document.querySelectorAll(".operation")
 
 let calculatorMemory = [];
-// let 
 let subDisplay="";
 let mainDisplay = "";
 let solution ="";
 
 const spliceAndReplace = (solution) => {
    calculatorMemory.splice(0,3,solution)
-   console.log(calculatorMemory)
-   console.log("spliceAndReplace is running")
+   // console.log(calculatorMemory)
+   // console.log("spliceAndReplace is running")
 }
 
 const itsMathsTime = () => {
    if (calculatorMemory[1]===key__divide.innerHTML){
-      // if (calculatorMemory[0]===""){
-      // }
-      // else{
       const division = `${Number(calculatorMemory[0])/Number(calculatorMemory[2])}`
          spliceAndReplace(division)
-      // }
    }else if (calculatorMemory[1]===key__multiply.innerHTML){
       const multiplication = `${Number(calculatorMemory[0])*Number(calculatorMemory[2])}`
          spliceAndReplace(multiplication)
@@ -90,15 +72,20 @@ if (mainDisplay.includes(".")){
 })
 
 key__minus.addEventListener("click",()=> {
-if (mainDisplay===""){
+if (mainDisplay.indexOf("-")===-1){
    mainDisplay = "-" + `${mainDisplay}`
    finalDisplay.innerHTML = `${mainDisplay}`
-   subDisplay=`${subDisplay}` + "-"
+   if (subDisplay.includes(mainDisplay.substring(1,mainDisplay.length))){
+      subDisplay=`${subDisplay.substring(0,subDisplay.length - mainDisplay.length+1)}` + `${mainDisplay}`
+      workingDisplay.innerHTML = `${subDisplay}`
+   }else {
+      subDisplay=`${mainDisplay}`
+      workingDisplay.innerHTML = `${subDisplay}`
+   }
+}else if (mainDisplay.includes("-")){
+   subDisplay=subDisplay.substring(0, subDisplay.indexOf("-")) + subDisplay.substring(subDisplay.indexOf("-")+1, subDisplay.length);
    workingDisplay.innerHTML = `${subDisplay}`
-}else if (mainDisplay==="-"){
-   subDisplay=subDisplay.substring(0, subDisplay.length - 1);
-   workingDisplay.innerHTML = `${subDisplay}`
-   mainDisplay="";
+   mainDisplay=mainDisplay.substring(1, mainDisplay.length);;
    finalDisplay.innerHTML = `${mainDisplay}`
 }
 })
@@ -113,7 +100,7 @@ for (let i = 0; i<operation.length;i++) {
       calculatorMemory.push(mainDisplay)
       calculatorMemory.push(operation[i].innerHTML)
       mainDisplay="";
-      console.log(calculatorMemory)
+      // console.log(calculatorMemory)
       if (subDisplay.charAt(subDisplay.length-1)===key__multiply.innerHTML ||
       subDisplay.charAt(subDisplay.length-1)===key__divide.innerHTML ||
       subDisplay.charAt(subDisplay.length-1)===key__add.innerHTML||
@@ -129,8 +116,8 @@ for (let i = 0; i<operation.length;i++) {
       }else if(calculatorMemory.length===4 && calculatorMemory[2]===""){
          newOperator=calculatorMemory[3]
          calculatorMemory.splice(1,3,newOperator)
-         console.log(calculatorMemory)
-         console.log("operator changed")
+         // console.log(calculatorMemory)
+         // console.log("operator changed")
       }
    })
 }
@@ -151,8 +138,9 @@ key__equal.addEventListener("click",()=> {
       finalDisplay.innerHTML = `${mainDisplay}`
       calculatorMemory=[];
    }
-   mainDisplay=""
-   subDisplay="";
+   // mainDisplay=""
+   // subDisplay="";
+
 })
 
 // ==========================
@@ -169,7 +157,7 @@ key__delete.addEventListener("click",()=> {
    }else {
    subDisplay=subDisplay.substring(0, subDisplay.length - 1);
    workingDisplay.innerHTML = `${subDisplay}`
-   console.log("delete button pressed")
+   // console.log("delete button pressed")
    }
 })
 
@@ -179,7 +167,7 @@ key__delete.addEventListener("click",()=> {
 // ==========================
 
 key__clear.addEventListener("click",()=> {
-   console.log("clear button pressed")
+   // console.log("clear button pressed")
    mainDisplay="";
    subDisplay="";
    calculatorMemory=[];
